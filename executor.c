b3f04a3c1d66e6595196d35b089aecc37a31859b
akanazi0 <abdulelah.khma@gmail.com>
2026-09-01 12:44:01 +0300

    Task 4: Simple shell 0.3 feat: integrate PATH resolution into executor for standard commands

--- executor.c
+++ executor.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * exec_cmd - Executes parsed arguments in a child process
+ * exec_cmd - Executes parsed arguments by resolving PATH and forking
  * @args: Array of argument strings
  * @prog_name: Name of the shell executable (for error reporting)
  * @count: Current line count for error reporting
@@ -10,21 +10,31 @@
  */
 void exec_cmd(char **args, char *prog_name, int count)
 {
+	char *actual_cmd;
 	pid_t child_pid;
 	int status;
+
+	actual_cmd = find_path(args[0]);
+	if (!actual_cmd)
+	{
+		fprintf(stderr, "%s: %d: %s: not found\n",
+			prog_name, count, args[0]);
+		return;
+	}
 
 	child_pid = fork();
 	if (child_pid == -1)
 	{
 		perror("fork");
+		free(actual_cmd);
 		return;
 	}
 	else if (child_pid == 0)
 	{
-		if (execve(args[0], args, environ) == -1)
+		if (execve(actual_cmd, args, environ) == -1)
 		{
-			fprintf(stderr, "%s: %d: %s: not found\n",
-				prog_name, count, args[0]);
+			perror(prog_name);
+			free(actual_cmd);
 			exit(127);
 		}
 	}
@@ -32,4 +42,6 @@ void exec_cmd(char **args, char *prog_name, int count)
 	{
 		wait(&status);
 	}
+
+	free(actual_cmd);
 }
